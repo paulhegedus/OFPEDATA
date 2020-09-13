1512a15826d11ac0214139d4418bac86c98ecf18
paulheg <paulhegedus@montana.edu>
2020-09-13 02:25:00 -0600

    Add files via upload

---
+++ R/sec1east_bbox.R
@@ -0,0 +1,10 @@
+#' @title sec1east_bbox
+#' @description Field boundary for Farmer B's OFPE experimental field
+#' named 'sec1east'.
+#' @format A data frame with 1 rows and 2 variables:
+#' \describe{
+#'   \item{\code{id}}{double ID of boundary.}
+#'   \item{\code{geometry}}{list Coordinates of bounding box polygon.}
+#'}
+#' @details DETAILS
+"sec1east_bbox"
